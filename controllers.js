fe4cf880370d8ad3e3ddcf2b68e5c9681e3c89d7
Erik <elaktrato@gmail.com>
2021-10-09 09:52:58 +0200

    egh

--- controllers.js
+++ controllers.js
@@ -1,7 +1,47 @@
+const pgp = require('pg-promise')();
+console.log(process.env.DB_PASS);
+const username = process.env.DB_USER;
+const password = process.env.DB_PASS;
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+
+const uri = `${username}:${password}@${host}:${port}/library`
+
+console.log(uri);
+const db = pgp(uri)
+
 async function addBook(book) {
-    return book;
+    const newBook = {
+        title: book.title,
+        description: book.description,
+        author_id: book.authorId
+    }
+
+    const result = await db.query('INSERT INTO books(${this:name}) VALUE(${this:csv})', newBook)
+    return result;
+}
+
+async function getBooks() {
+
+    const result = await db.query('SELECT ${columns:name} FROM ${table:name}', {
+        columns: ['title', 'description'],
+        table: 'books'
+    });
+    return result;
+}
+
+async function getSingleBook(id) {
+
+    const result = await db.query('SELECT ${columns:name} FROM ${table:name} WHERE id = $/bookid/', {
+        columns: ['id', 'title'],
+        table: 'books',
+        bookid: id
+    });
+    return result;
 }
 
 module.exports = {
-    addBook
+    addBook,
+    getBooks,
+    getSingleBook
 }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { addBook } = require('./controllers');
-const cors = require('cors')
+require('dotenv').config();
+const { addBook, getBooks, getSingleBook } = require('./controllers');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
@@ -8,11 +9,21 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-app.post("/book", async(req, res) => {
+app.post("/books", async(req, res) => {
     const newBook = req.body;
     const result = await addBook(newBook);
     res.status(201).send(result)
 });
+
+app.get("/books", async(req, res) => {
+    const result = await getBooks();
+    res.status(200).send(result)
+})
+
+app.get("/books/:id", async(req, res) => {
+    const result = await getSingleBook(req.params.id);
+    res.status(200).send(result)
+})
 
 app.listen(PORT, () => {
     console.log("Book API running on " + PORT)
